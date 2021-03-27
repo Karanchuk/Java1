0061ce0395f8118391832cd3444e759d4954f475
Bohdan Karanchuk <b.karanchuk@gmail.com>
2021-03-27 20:24:23 +0300

    Revert "Выполнены все пункты задания из Урок 2. Основные конструкции" По ошибке выполнеил не то домашнее задание. Так и не понял, откуда взялись эти пункты, но в момент создания pull request обратил внимание, что текст задания совсем не тот. This reverts commit 1064946fe7a7500e95086575b49e7dc101708081.

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,11 +1,9 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
-        
     }
 
     static void printThreeWords() {
@@ -46,64 +44,4 @@ public class HomeWorkApp {
             System.out.println("a < b");
         }
     }
-
-    /*1. Написать метод, принимающий на вход два целых числа
-    * и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
-    * если да – вернуть true, в противном случае – false.
-    */
-    static boolean isInRange(int a, int b) {
-        boolean result = false;
-        if (a + b >= 10 && a + b <= 20) {
-            result = true;
-        }
-        return result;
-    }
-
-    /*2. Написать метод, которому в качестве параметра передается целое число,
-    * метод должен напечатать в консоль, положительное ли число передали или отрицательное.
-    * Замечание: ноль считаем положительным числом.
-    */
-    static void writeIsPositiveOrNegative(int number) {
-        System.out.println("Число " + (isNegativeNumber(number) ? "отрицательное" : "положительное"));
-    }
-    /*3. Написать метод, которому в качестве параметра передается целое число.
-    * Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
-    */
-    static boolean isNegativeNumber(int number) {
-        boolean numberSign;
-        if (number < 0) {
-            numberSign = true;
-        } else {
-            numberSign = false;
-        }
-        return numberSign;
-    }
-    /*4. Написать метод, которому в качестве аргументов передается строка и число,
-    * метод должен отпечатать в консоль указанную строку, указанное количество раз;
-    * */
-    static void printLine(String line, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.println(line);
-        }
-    }
-    /*5. * Написать метод, который определяет, является ли год високосным,
-    * и возвращает boolean (високосный - true, не високосный - false).
-    * Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-    */
-    static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-

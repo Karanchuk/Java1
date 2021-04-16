c0ccf07b2558a1bc12e90222d1aba8e4bbd0bfee
Bohdan Karanchuk <b.karanchuk@gmail.com>
2021-04-16 17:58:43 +0300

    Выполнены все пункты задания из Урок 7. Практика ООП и работа со строками

---
+++ Cat.java
@@ -0,0 +1,30 @@
+package HW_7;
+
+public class Cat {
+    private final String name;
+    private int appetite;
+    private boolean satiety = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void eat(Plate p) {
+        if (p.decreaseFood(appetite)) {
+            satiety = true;
+        }
+    }
+}
